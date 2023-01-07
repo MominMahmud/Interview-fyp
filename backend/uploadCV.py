1bb93e318ddfa05e7beea96f51ee32e19b2708e6
Momin Mahmud <mominmahmudjalib@gmail.com>
2023-01-07 17:22:26 +0500

    working on uplaodCV.py

--- backend/uploadCV.py
+++ backend/uploadCV.py
@@ -1,1 +1,19 @@
-from flask import Flask, request, url_for
+from flask import Flask, Response, request, render_template
+import pymongo
+from pymongo import MongoClient
+import json
+import dns
+
+
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+try:
+    mongo = pymongo.MongoClient(
+        "mongodb+srv://admin:1234@fyp.dhi0fxe.mongodb.net/test?authMechanism=DEFAULT"
+    )
+    db = mongo["fastHire"]
+    mongo.server_info()
+except:
+    print("Couldn't connect to Db")
