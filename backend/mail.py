3ee22e332f5d65b72b3474868767eeadac09078c
Momin Mahmud <mominmahmudjalib@gmail.com>
2023-01-08 00:19:18 +0500

    Mail is finalized emailSend function is done, CV is being uploaded

--- backend/mail.py
+++ backend/mail.py
@@ -17,11 +17,11 @@ mail = Mail(app)
 def hello():
     if request.method == "POST":
         msg = Message(
-            "Hey", sender="noreply@demo.com", recipients=["i190633@nu.edu.pk"]
+            "FastHire", sender="noreply@demo.com", recipients=["i190633@nu.edu.pk"]
         )
-        msg.body = "From my NU id MOmin"
+        msg.body = "Dear Candidate,\n\n\nYour details have been received by us successfuly. Your information is confidential and will not be shared to anyone.\nYour interview will be conducted by using your camera and microphone. When prompted to allow camera or video click Allow. Whenever you are ready to give the interview,\nClick on this link below: \n\nhttp://localhost:3000/interview.\n\n\n\nInstructions:\n1- Click on Listen button to hear the question.\n2- Click on Record to record your response.\n3- Click on Save Answer to save your answer and see the text deteced by us.\n4-Click on Next Question to switch to the next question. Click listen button again to listen to the new question.\n5-Click on Submit to submit your answer."
         mail.send(msg)
-        return "Sent Email!"
+        return render_template("emailSent.html")
 
     return render_template("index.html")
 
