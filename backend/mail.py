1d5f225cd2670498f891cfbbe816e42af4e0c77c
Momin Mahmud <mominmahmudjalib@gmail.com>
2023-01-08 00:28:26 +0500

    removed password again

--- backend/mail.py
+++ backend/mail.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USERNAME"] = "i190633@nu.edu.pk"
-app.config["MAIL_PASSWORD"] = "Metalmgsv@5"
+app.config["MAIL_PASSWORD"] = "\"
 app.config["MAIL_USE_TLS"] = False
 app.config["MAIL_USE_SSL"] = True
 mail = Mail(app)
@@ -17,7 +17,9 @@ mail = Mail(app)
 def hello():
     if request.method == "POST":
         msg = Message(
-            "FastHire", sender="noreply@demo.com", recipients=["i190633@nu.edu.pk"]
+            "FastHire",
+            sender="noreply@demo.com",
+            recipients=["i190633@nu.edu.pk"],
         )
         msg.body = "Dear Candidate,\n\n\nYour details have been received by us successfuly. Your information is confidential and will not be shared to anyone.\nYour interview will be conducted by using your camera and microphone. When prompted to allow camera or video click Allow. Whenever you are ready to give the interview,\nClick on this link below: \n\nhttp://localhost:3000/interview.\n\n\n\nInstructions:\n1- Click on Listen button to hear the question.\n2- Click on Record to record your response.\n3- Click on Save Answer to save your answer and see the text deteced by us.\n4-Click on Next Question to switch to the next question. Click listen button again to listen to the new question.\n5-Click on Submit to submit your answer."
         mail.send(msg)
