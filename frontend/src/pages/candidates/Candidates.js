4211b11d222976706248dd5a3eded8844aef1348
Waleed <waleed.mukhtar1999@gmail.com>
2023-04-14 00:43:43 +0500

    fit kiya sab kch

--- frontend/src/pages/candidates/Candidates.js
+++ frontend/src/pages/candidates/Candidates.js
@@ -5,6 +5,55 @@ import Modal from "@mui/material/Modal";
 import {useState,useEffect} from "react"
 import axios from "axios";
 export default function Candidates() {
+  const Data = [
+    {
+      id: 1,
+      year: 2016,
+      userGain: 80000,
+      userLost: 823
+    },
+    {
+      id: 2,
+      year: 2017,
+      userGain: 45677,
+      userLost: 345
+    },
+    {
+      id: 3,
+      year: 2018,
+      userGain: 78888,
+      userLost: 555
+    },
+    {
+      id: 4,
+      year: 2019,
+      userGain: 90000,
+      userLost: 4555
+    },
+    {
+      id: 5,
+      year: 2020,
+      userGain: 4300,
+      userLost: 234
+    }
+  ];
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "&quot;#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]})
   const style = {
     position: "absolute",
     top: "50%",
